b105be1d23f3f9cee18d3ab6b194b5553f222edf
CHANDANKUMAR45 <167713141+CHANDANKUMAR45@users.noreply.github.com>
2025-12-02 08:24:14 +0000

    update day28 folder

---
+++ Day28/index.js
@@ -0,0 +1,15 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({apiKey:"AIzaSyAJA8E3Oeh-kn4B-EomrapuXhASPg6itew"});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "How are you",
+  });
+  console.log(response.text);
+}
+
+await main();
+
+// 

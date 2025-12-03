d43404de46365200afb3ffeef1810ee40667ea66
CHANDANKUMAR45 <167713141+CHANDANKUMAR45@users.noreply.github.com>
2025-12-03 12:44:30 +0000

    update

--- Day28/aichat.js
+++ Day28/aichat.js
@@ -1,14 +1,15 @@
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenAI } from "@google/generative-ai";
 
 const ai = new GoogleGenAI({apiKey:"AIzaSyAJA8E3Oeh-kn4B-EomrapuXhASPg6itew"});
 
-async function main() {
+async function main(msg) {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: "How are you",
+    contents: msg
   });
-  console.log(response.text);
+  return response.text;
 }
 
-await main();
+// await main();
 
+module.exports=main;
--- Day28/index.js
+++ Day28/index.js
@@ -1,16 +1,21 @@
-const express =require('express');
+import expess from "express";
+import main from "./aichat.js";
 const app=express();
 
 app.use(express.json());
 
-app.post('/chat')
+app.post("/chat",async(req,res)=>{
+  const {msg}=req.body;
 
+ const ans= await main(msg);
 
+ res.send(ans);
 
+});
 
 
 app.listen(3000,()=>{
   console.log("Listening at port 3000");
   
-})
+});
 
